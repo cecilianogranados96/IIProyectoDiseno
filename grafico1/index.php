dc267b88d8f15461012de932fb84528f2e5f7367
José Andrés Ceciliano Granados <cecilianogranados96@hotmail.com>
2018-11-03 13:45:13 -0600

    [Cambios]

--- grafico1/index.php
+++ grafico1/index.php
@@ -4,51 +4,18 @@
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <link href="assets/css/filtros.css" rel="stylesheet" >
-    <script src="assets/js/filtros.js"></script>
+    <link href="../assets/css/filtros.css" rel="stylesheet" >
+    <script src="../assets/js/filtros.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <style>
+        body{
+            background-color: #e3e3e3;
+        }
+        </style>
 </head>
 
 <body>
-      <div class="container">
- <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Project name</a>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
-              <li><a href="../navbar-static-top/">Static top</a></li>
-              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
-
+ 
     
 
         <div class="row">
@@ -86,8 +53,6 @@
              <script type="text/javascript">
       google.charts.load("current", {
         "packages":["map"],
-        // Note: you will need to get a mapsApiKey for your project.
-        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
         "mapsApiKey": "AIzaSyCum0ulO0WQmf1cNz7C9CDjsBgdkBqecuQ"
     });
       google.charts.setOnLoadCallback(drawChart);
@@ -95,7 +60,7 @@
         var data = google.visualization.arrayToDataTable([
           ['Lat', 'Long', 'Name'],          
             <?php 
-                $host = "35.232.101.189";
+                $host = "35.230.49.154";
                 $contrasena ="diseno123*";
                 $usuario = "admin_diseno";
                 $base ="admin_diseno";
@@ -134,7 +99,7 @@
           var data = google.visualization.arrayToDataTable([
          ['City',   'Population', 'Area'],     
             <?php 
-                $host = "35.232.101.189";
+                $host = "35.230.49.154";
                 $contrasena ="diseno123*";
                 $usuario = "admin_diseno";
                 $base ="admin_diseno";
@@ -165,7 +130,7 @@
             
             
 
-        </div>
+   
     </div>
 </body>
 
